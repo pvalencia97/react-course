75bd0c2906d2e41a3baa2f32a798749faad47a9e
pvalencia97 <76021700+pvalencia97@users.noreply.github.com>
2025-01-27 17:57:42 -0600

    Update index.jsx

--- src/Componentes/Navbar/index.jsx
+++ src/Componentes/Navbar/index.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
       </ul>
       <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
-          teff@platzi.com
+          JValencia.97.ramirez@gmail.com
         </li>
         <li>
           <NavLink
